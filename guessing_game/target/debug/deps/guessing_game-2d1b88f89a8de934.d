be4c479e04b3d0a43d4c93b13c5750cd1e24d37e
MattSHarker <mattsharker@gmail.com>
2019-07-31 15:49:24 -0700

    added rand crate

---
+++ guessing_game/target/debug/deps/guessing_game-2d1b88f89a8de934.d
@@ -0,0 +1,5 @@
+C:\msys64\home\Matthew\github\rust_exercises\guessing_game\target\debug\deps\guessing_game-2d1b88f89a8de934.rmeta: src\main.rs
+
+C:\msys64\home\Matthew\github\rust_exercises\guessing_game\target\debug\deps\guessing_game-2d1b88f89a8de934.d: src\main.rs
+
+src\main.rs:
