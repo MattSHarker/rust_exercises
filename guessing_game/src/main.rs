91ba20073aeabbfaad15c4351d860ce73dc4f2bf
MattSHarker <mattsharker@gmail.com>
2019-07-31 18:16:55 -0700

    finalized code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,22 +5,16 @@ use std::cmp::Ordering; // allows use of Ordering
 use rand::Rng;          // allows random number generation
 
 fn main() {
-    // display an introduction
     println!("Guess the number!");
 
-    // create a non-mutable random number 1 <= x < 101
+    // create a random number 1 <= x < 101
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    // display the random number (for debugging)
-    // println!("The secret number is: {}", secret_number);
 
     // start an infinite loop
     loop {
-        // tell the user what to do
         println!("Please input your guess:");
 
-        // create a mutable variable "guess" and
-            // assign it an instance of an empty string 
+        // create a variable "guess" and assign it an instance of an empty string 
         let mut guess = String::new();
 
         // read input from the user and assign it to "guess"
