c67888f119260ee64f1abb6ef0ffc7b78a2d952e
mattsharker <mattsharker@gmail.com>
2019-09-06 12:55:07 -0700

    Updated ch8-vector with more examples

--- chapter_8/vector/src/main.rs
+++ chapter_8/vector/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let v1: Vec<i32> = Vec::new();
     let v2 = vec![1, 2, 3]; // when you know what's going into the vector
 
+
     // how to add to a vector
     let mut v3 = Vec::new();
     v3.push(1);  // .push(x) adds a value to the vector
@@ -13,18 +14,44 @@ fn main() {
     v3.push(4);
     v3.push(5);
 
+
     // how to read values from a vector
     let third: &i32 = &v3[2];   // rust is indexed starting at 0
     let fourth: Option<&i32> = v3.get(3);
     println!("third: {}, fourth: {:?}", third, fourth);
 
+
     // what happens when a non-existent value is read
     // let does_not_exist: &i32 = &v3[100];    // this will throw an error, good if you don't want the program to continue if too much is accessed
     let does_not_exist: Option<&i32> = v3.get(100);
+
 
     // iterating over values in a vector
     let v4 = vec![100, 101, 102, 103, 104];
     for i in &v4 {  // change "&v4" to "&mut v4" to modify values in the vector
         println!("{}", i);
+    }
+
+
+    // using an Enum to store multiple types
+    // first, create an enum with multiple types
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    // then create a vector that holds the enum
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(3.141),
+        SpreadsheetCell::Text(String::from("x^2-3y")),
+    ];
+    // each instance of the enum can hold different types, but the vector
+        // only cares that they're all the same enum type
+
+    for i in &row {
+        println!("{:?}", i);
     }
 }
